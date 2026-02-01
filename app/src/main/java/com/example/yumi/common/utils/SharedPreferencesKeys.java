e80460e1038aa79e9b86df376d7da8729bbe903c
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-02-01 02:07:02 +0200

    Add SharedPreferencesKeys util class.

---
+++ app/src/main/java/com/example/yumi/common/utils/SharedPreferencesKeys.java
@@ -0,0 +1,6 @@
+package com.example.yumi.common.utils;
+
+public abstract class SharedPreferencesKeys {
+    public static final String PREF_NAME =  "yumi_prefs";
+    public static final String KEY_ONBOARDING_COMPLETED =  "onboarding_completed";
+}
