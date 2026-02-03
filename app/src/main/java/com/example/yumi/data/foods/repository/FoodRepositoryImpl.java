3ec9e06f85210d18b05689994bc1126b1fbaddc7
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-02-03 21:55:11 +0200

    Convert retrofit client to singleton enum.

--- app/src/main/java/com/example/yumi/data/foods/repository/FoodRepositoryImpl.java
+++ app/src/main/java/com/example/yumi/data/foods/repository/FoodRepositoryImpl.java
@@ -1,6 +1,13 @@
 package com.example.yumi.data.foods.repository;
 
+import com.example.yumi.data.foods.datasources.remote.FoodRemoteDataSource;
+import com.example.yumi.data.foods.datasources.remote.FoodRemoteDataSourceImpl;
 import com.example.yumi.domain.foods.repository.FoodRepository;
 
 public class FoodRepositoryImpl implements FoodRepository {
+    private final FoodRemoteDataSource foodRemoteDataSource;
+
+    public FoodRepositoryImpl(){
+        foodRemoteDataSource = new FoodRemoteDataSourceImpl();
+    }
 }
---
+++ app/src/main/java/com/example/yumi/data/network/RetrofitClient.java
@@ -0,0 +1,40 @@
+package com.example.yumi.data.network;
+import com.example.yumi.data.config.APIConfig;
+import com.example.yumi.data.foods.datasources.remote.api.FoodServiceAPI;
+import java.util.concurrent.TimeUnit;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public enum RetrofitClient {
+    INSTANCE;
+
+    private final FoodServiceAPI api;
+
+    RetrofitClient() {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(APIConfig.BASE_URL)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .build();
+
+        api = retrofit.create(FoodServiceAPI.class);
+    }
+
+    public FoodServiceAPI getFoodApi() {
+        return api;
+    }
+}
