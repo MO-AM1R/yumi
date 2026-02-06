195c96a72221946770bde3d8cf246bb0968de20a
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-02-06 03:02:55 +0200

    Add AuthContract.

--- app/src/main/java/com/example/yumi/presentation/authentication/AuthContract.java
+++ app/src/main/java/com/example/yumi/presentation/authentication/AuthContract.java
@@ -3,11 +3,60 @@ import com.example.yumi.presentation.base.BaseView;
 
 
 public interface AuthContract {
-    interface View extends BaseView {
-
+    interface SplashView extends BaseView{
+        void navigateToOnboarding();
+        void navigateToLogin();
+        void navigateToHome();
     }
 
-    interface Presenter {
+    interface SplashPresenter {
+        void attachView(SplashView view);
+        void detachView();
+        void onAnimationsComplete();
+    }
 
+    interface OnboardingView extends BaseView {
+        void showPage(int index);
+        void navigateToLogin();
+        int getCurrentPageIndex();
+        int getTotalPages();
+    }
+
+    interface OnboardingPresenter {
+        void attachView(OnboardingView view);
+        void detachView();
+        void onNextClicked(int currentIndex);
+        void onSkipClicked();
+    }
+
+    interface LoginView extends BaseView {
+        void showEmailError(String error);
+        void showPasswordError(String error);
+        void clearErrors();
+        void onLoginSuccess();
+        void showSuccess(String message);
+    }
+
+    interface LoginPresenter {
+        void attachView(LoginView view);
+        void detachView();
+        void login(String email, String password);
+        void loginWithGoogle(android.content.Context context);
+    }
+
+    interface SignUpView extends BaseView {
+        void showUsernameError(String error);
+        void showEmailError(String error);
+        void showPasswordError(String error);
+        void clearErrors();
+        void onSignUpSuccess();
+        void showSuccess(String message);
+    }
+
+    interface SignUpPresenter {
+        void attachView(SignUpView view);
+        void detachView();
+        void signUp(String name, String email, String password);
+        void signUpWithGoogle(android.content.Context context);
     }
 }
