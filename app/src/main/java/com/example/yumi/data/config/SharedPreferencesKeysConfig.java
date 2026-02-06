2d95767847d54fa55a347afef8dce04e80e2ab2d
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-02-06 03:01:36 +0200

    Update SharedPreferencesKeysConfig.

--- app/src/main/java/com/example/yumi/data/config/SharedPreferencesKeysConfig.java
+++ app/src/main/java/com/example/yumi/data/config/SharedPreferencesKeysConfig.java
@@ -4,4 +4,7 @@ public abstract class SharedPreferencesKeysConfig {
     public static final String PREF_NAME =  "yumi_prefs";
     public static final String KEY_ONBOARDING_COMPLETED =  "onboarding_completed";
     public static final String KEY_LOGGED_IN =  "logged_in";
+    public static final String KEY_USER_ID = "uid";
+    public static final String KEY_DARK_MODE = "darkMode";
+    public static final String KEY_LANGUAGE = "language";
 }
