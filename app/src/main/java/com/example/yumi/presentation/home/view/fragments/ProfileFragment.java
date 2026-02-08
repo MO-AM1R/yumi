dcf35aa1957397353f0fd7562585013e6eb92b31
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-02-08 12:55:36 +0200

    Implement Profile screen UI.

--- app/src/main/java/com/example/yumi/presentation/home/view/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/yumi/presentation/home/view/fragments/ProfileFragment.java
@@ -1,20 +1,66 @@
 package com.example.yumi.presentation.home.view.fragments;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.yumi.R;
+import com.example.yumi.databinding.FragmentProfileBinding;
 
 
 public class ProfileFragment extends Fragment {
+    private FragmentProfileBinding binding;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        if (view == null){
+            binding = FragmentProfileBinding.inflate(inflater);
+            view = binding.getRoot();
+        }else{
+            binding = FragmentProfileBinding.bind(view);
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setupLanguageSpinner();
+    }
+
+    private void setupLanguageSpinner() {
+        Spinner spinner = binding.spinnerLanguage;
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(requireContext(),
+                R.array.languages_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("TAG", "Position: " + position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
