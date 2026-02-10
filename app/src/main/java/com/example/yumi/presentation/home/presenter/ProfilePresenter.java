f10245eb77467d000cbe2d72e38272d3014ab670
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-02-10 20:13:26 +0200

    Add favorite counter in profile screen.

--- app/src/main/java/com/example/yumi/presentation/home/presenter/ProfilePresenter.java
+++ app/src/main/java/com/example/yumi/presentation/home/presenter/ProfilePresenter.java
@@ -1,17 +1,15 @@
 package com.example.yumi.presentation.home.presenter;
-
 import android.content.Context;
-
 import androidx.appcompat.app.AppCompatDelegate;
-
+import com.example.yumi.data.favorite.repository.FavoriteRepositoryImpl;
 import com.example.yumi.data.user.repository.UserRepositoryImpl;
+import com.example.yumi.domain.favorites.repository.FavoriteRepository;
 import com.example.yumi.domain.user.model.User;
 import com.example.yumi.domain.user.repository.UserRepository;
 import com.example.yumi.presentation.base.BasePresenter;
 import com.example.yumi.presentation.home.contract.ProfileContract;
 import com.example.yumi.utils.LocaleHelper;
 import com.example.yumi.utils.ThemeHelper;
-
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
@@ -20,18 +18,20 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 public class ProfilePresenter extends BasePresenter<ProfileContract.View>
         implements ProfileContract.Presenter {
     private final UserRepository userRepository;
+    private final FavoriteRepository favoriteRepository;
     private final Context applicationContext;
 
     public ProfilePresenter(Context context) {
         this.applicationContext = context;
         userRepository = new UserRepositoryImpl(context);
+        favoriteRepository = new FavoriteRepositoryImpl(context);
     }
 
-    private void onUserDetailsLoaded(User user, int plannedMeals) {
+    private void onUserDetailsLoaded(User user, int favoriteMeals) {
         withView(v -> {
             v.hideLoading();
             v.showUserDetails(user);
-            v.showUserPlannedMealsCounter(plannedMeals);
+            v.showUserFavoriteMealsCounter(favoriteMeals);
         });
     }
 
@@ -40,13 +40,13 @@ public class ProfilePresenter extends BasePresenter<ProfileContract.View>
         withView(v -> {
             v.showLoading();
 
-            Disposable disposable = userRepository.getMealPlan()
+            User user = userRepository.getCurrentUser();
+            Disposable disposable = favoriteRepository.getAllFavoriteIds()
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(
-                            mealPlan ->
-                                    onUserDetailsLoaded(userRepository.getCurrentUser(),
-                                            mealPlan.getDays().size())
+                            strings -> onUserDetailsLoaded(user, strings.size()),
+                            throwable -> v.showError(throwable.getLocalizedMessage())
                     );
 
             compositeDisposable.add(disposable);
