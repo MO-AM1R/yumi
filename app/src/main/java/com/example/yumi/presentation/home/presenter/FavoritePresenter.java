0081ceaf84621f17b0e4c0a8dcb947faaf68ce27
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-02-10 04:16:56 +0200

    Implement delete favorites in favorites screen.

--- app/src/main/java/com/example/yumi/presentation/home/presenter/FavoritePresenter.java
+++ app/src/main/java/com/example/yumi/presentation/home/presenter/FavoritePresenter.java
@@ -1,7 +1,5 @@
 package com.example.yumi.presentation.home.presenter;
 import android.content.Context;
-import android.util.Log;
-
 import com.example.yumi.data.favorite.repository.FavoriteRepositoryImpl;
 import com.example.yumi.domain.favorites.repository.FavoriteRepository;
 import com.example.yumi.domain.meals.model.Meal;
@@ -53,7 +51,12 @@ public class FavoritePresenter extends BasePresenter<FavoriteContract.View>
 
     @Override
     public void onMealRemoved(Meal meal) {
+        Disposable disposable = repository.deleteMeal(meal.getId())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe();
 
+        compositeDisposable.add(disposable);
     }
 
     @Override
