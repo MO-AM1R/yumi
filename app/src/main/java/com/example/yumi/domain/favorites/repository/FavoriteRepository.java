94959037dfe59a722bd0e2ff8512726b5f3dba59
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-02-10 19:09:09 +0200

    Update Favorite repository to latest update.

--- app/src/main/java/com/example/yumi/domain/favorites/repository/FavoriteRepository.java
+++ app/src/main/java/com/example/yumi/domain/favorites/repository/FavoriteRepository.java
@@ -1,20 +1,24 @@
 package com.example.yumi.domain.favorites.repository;
-
 import com.example.yumi.domain.meals.model.Meal;
-
 import java.util.List;
 import java.util.Set;
-
 import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.core.Single;
 
+
 public interface FavoriteRepository {
-    public Completable insertMeal(Meal meal) ;
-    public Completable deleteMeal(String mealId);
-    public Single<Boolean> onFavMeal(Meal meal) ;
-    public Completable clearAllMeals();
-    public Flowable<List<Meal>> getAllFavoriteMeals();
-    public Flowable<Boolean> isFav(String mealId);
+    Completable insertMeal(Meal meal);
+
+    Completable deleteMeal(String mealId);
+
+    Single<Boolean> onFavMeal(Meal meal);
+
+    Completable clearAllFavorites();
+
+    Flowable<List<Meal>> getAllFavoriteMeals();
+
+    Flowable<Boolean> isFav(String mealId);
+
     Single<Set<String>> getAllFavoriteIds();
 }
